475916e83427f8d3a44a009f48c10c782d5147f9
Dmitry Arkhipov <arhidiman@mail.ru>
2025-03-26 21:52:50 +0700

    refactor: изменил вёрстку формы создания записи

--- src/modules/TableRecordForm/TableRecordForm.tsx
+++ src/modules/TableRecordForm/TableRecordForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { RootState, useAppDispatch, useAppSelector } from "@/main";
-import { Table, TableBody, TableCell, TableContainer, TableRow, TextField, Button, Paper, FormControl, FormHelperText, Typography } from "@mui/material";
+import { TableHead, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Button, Paper, FormControl, FormHelperText, Typography } from "@mui/material";
 import { setRecordAction } from "@/pages";
 import { createTableItem } from "@/ApiClient/ApiClient";
 import type { ITableItemDto } from "@/ApiClient/dto";
@@ -29,7 +29,7 @@ export const TableRecordForm: React.FC = () => {
     const validate = () => {
         const newErrors: Record<string, string> = {};
         Object.keys(recordFieldsMap).forEach((key) => {
-            if (!record[key]) {
+            if (!record[key as keyof Omit<ITableItemDto, 'id'>]) {
                 newErrors[key] = "Это поле обязательно для заполнения";
             }
         });
@@ -38,48 +38,48 @@ export const TableRecordForm: React.FC = () => {
     };
 
     const handleSave = async () => {
-        setIsFormSubmitted(true); // Форма была отправлена
-        if (!validate()) return;
-
+        if (!validate()) return
+        setIsFormSubmitted(true)
         await createTableItem(record);
-    };
+    }
 
-    const renderFormFields = () => {
-        return (Object.keys(recordFieldsMap) as (keyof Omit<ITableItemDto, "id">)[]).map((key) => (
-            <TableRow key={key}>
-                <TableCell>
-                    <Typography variant="body2" color="textPrimary">
-                        {recordFieldsMap[key]} <span style={{ color: "red" }}>*</span>
-                    </Typography>
-                </TableCell>
+    const tableHeaderCells = () => {
+        return Object.values(recordFieldsMap).map((colName, i) => <TableCell key={i}>{colName}</TableCell>)
+    }
+    
+    const formRow = () => {
+        return Object.keys(recordFieldsMap).map(key => {
+            return (
                 <TableCell>
                     <FormControl fullWidth error={isFormSubmitted && Boolean(errors[key])} variant="outlined" size="small">
                         <TextField
                             required
-                            value={record[key]}
                             onChange={(e) => setRecord(key, e.target.value)}
                         />
                         {isFormSubmitted && errors[key] && <FormHelperText>{errors[key]}</FormHelperText>}
                     </FormControl>
                 </TableCell>
-            </TableRow>
-        ));
-    };
+            )
+        })
+    }
 
     return (
-        <TableContainer component={Paper} sx={{ maxWidth: 600, mt: 4 }}>
-            <Table>
-                <TableBody>
-                    {renderFormFields()}
-                    <TableRow>
-                        <TableCell colSpan={2} align="right">
-                            <Button variant="contained" color="primary" onClick={handleSave} sx={{ mr: 1 }}>
-                                Сохранить
-                            </Button>
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
-};
+        <TableContainer component={Paper}>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    {tableHeaderCells()}
+                </TableRow>
+            </TableHead>
+            <TableRow>
+                {formRow()}
+                <TableCell> 
+                    <Button variant="contained" color="primary" onClick={handleSave}>
+                        Создать запись
+                    </Button>
+                </TableCell>
+            </TableRow>
+        </Table>
+    </TableContainer>
+    )
+}
